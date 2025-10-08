6e890096e7a94c0fb36ec7efea8c8b1b702ae78c
pegn0ly <owafe001@gmail.com>
2025-10-08 14:39:36 +0300

    magic lines def

--- scripts/prepare_stage/spells_generation/core.lua
+++ scripts/prepare_stage/spells_generation/core.lua
@@ -1,14 +1,20 @@
 ---@alias MagicLineType
----|`FIRST_MAGIC_LINE`
----|`SECOND_MAGIC_LINE`
----|`THIRD_MAGIC_LINE`
----|`FOURTH_MAGIC_LINE`
----|`ADDITIONAL_MAGIC_LINE`
-FIRST_MAGIC_LINE = 1
-SECOND_MAGIC_LINE = 2
-THIRD_MAGIC_LINE = 3
-FOURTH_MAGIC_LINE = 4
-ADDITIONAL_MAGIC_LINE = 5
+---|`FIRST_MAIN_LINE`
+---|`SECOND_MAIN_LINE`
+---|`THIRD_MAIN_LINE`
+---|`RUNES_LINE`
+---|`WARCRIES_LINE`
+---|`FIRST_ADDITIONAL_LINE`
+---|`SECOND_ADDITIONAL_LINE`
+---|`THIRD_ADDITIONAL_LINE`
+FIRST_MAIN_LINE = 1
+SECOND_MAIN_LINE = 2
+THIRD_MAIN_LINE = 3
+RUNES_LINE = 4
+WARCRIES_LINE = 5
+FIRST_ADDITIONAL_LINE = 6
+SECOND_ADDITIONAL_LINE = 7
+THIRD_ADDITIONAL_LINE = 8
 
 spells_generation_core = {
     allowed_spells = {
@@ -41,6 +47,19 @@ spells_generation_core = {
         [MAGIC_SCHOOL_RUNIC] = range_generator.FromTop(SPELL_RUNE_OF_CHARGE, SPELL_RUNE_OF_DRAGONFORM),
         [MAGIC_SCHOOL_WARCRIES] = range_generator.FromTop(SPELL_WARCRY_RALLING_CRY, SPELL_WARCRY_SHOUT_OF_MANY)
     },
+
+    lines_data = {
+        [FIRST_MAIN_LINE] = {
+            [PLAYER_1] = {
+                x = 37,
+                y = 88,
+            },
+            [PLAYER_2] = {
+
+            },
+            count = 5
+        }
+    }   
 
     ---@type table<TownType, table<MagicLineType, SpellSchoolType>>
     lines_by_races = {
