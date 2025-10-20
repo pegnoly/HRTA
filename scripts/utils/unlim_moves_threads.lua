65e1ef33f43bd67d8e9a9a087aa657b274c03f3c
pegn0ly <owafe001@gmail.com>
2025-10-20 16:45:55 +0300

    move threads bug fixed

--- scripts/utils/unlim_moves_threads.lua
+++ scripts/utils/unlim_moves_threads.lua
@@ -9,7 +9,7 @@ unlim_moves_threads = {
     function (hero)
         while 1 do
             if unlim_moves_threads.heroes_states[hero] == MOVE_THREAD_TYPE_UNLIM then
-                ChangeHeroStat(hero, STAT_MOVE_POINTS, 100000)
+                ChangeHeroStat(hero, STAT_MOVE_POINTS, 10000)
             end
             sleep()
         end
@@ -19,7 +19,7 @@ unlim_moves_threads = {
     function (hero)
         while 1 do
             if unlim_moves_threads.heroes_states[hero] == MOVE_THREAD_TYPE_NO_MOVES then
-                ChangeHeroStat(hero, STAT_MOVE_POINTS, -100000)
+                ChangeHeroStat(hero, STAT_MOVE_POINTS, -10000)
             end
             sleep()
         end
@@ -27,21 +27,20 @@ unlim_moves_threads = {
 
     UpdateMoveThreadType = 
     function (hero, type)
-        unlim_moves_threads.heroes_states[hero] = type
+        consoleCmd("@unlim_moves_threads.heroes_states['"..hero.."'] = "..type.."")
     end
 }
 
 MapLoadingEvent.AddListener("HRTA_start_move_remove_listener",
 function ()
     for _, hero in GetObjectNamesByType("HERO") do
-        print("Hero start move remove ", hero)
-        ChangeHeroStat(hero, STAT_MOVE_POINTS, -100000)
+        ChangeHeroStat(hero, STAT_MOVE_POINTS, -10000)
     end
 end)
 
 AddHeroEvent.AddListener("HRTA_unlim_move_thread_add_hero_listener",
 function (hero)
-    unlim_moves_threads.heroes_states[hero] = MOVE_THREAD_TYPE_DEFAULT
+    consoleCmd("@unlim_moves_threads.heroes_states['"..hero.."'] = MOVE_THREAD_TYPE_DEFAULT") -- nival interactive
     startThread(unlim_moves_threads.UnlimMoveThread, hero)
     startThread(unlim_moves_threads.NoMovesThread, hero)
 end)
